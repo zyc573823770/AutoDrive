ebf9cf87e7ca2b7c918fc39050f24bb374c27f51
zyc573823770 <573823770@qq.com>
2021-03-29 18:51:39 +0800

    1

--- visual_module.py
+++ visual_module.py
@@ -7,6 +7,7 @@ import sys
 import time
 from copy import deepcopy
 import win32gui
+import math
 
 import cv2
 import numpy as np
@@ -21,6 +22,8 @@ from utils import normalize_
 import keys
 
 torch.backends.cudnn.benchmark = False
+GREEN = [0, 255, 0]
+WHITE = [255, 255, 255]
 
 def PostProcess(outputs, target_sizes):
     out_logits, out_curves = outputs['pred_logits'], outputs['pred_curves']
@@ -57,9 +60,6 @@ def get_lane_model():
     nnet.eval_mode()
     return nnet, input_size, mean, std
 
-def correct_windows():
-    pass
-
 def lane_detection(ori_image, mean, std, input_size, nnet, point=True):
     image = cv2.cvtColor(ori_image, cv2.COLOR_BGR2RGB)
     height, width = image.shape[0:2]
@@ -86,10 +86,9 @@ def lane_detection(ori_image, mean, std, input_size, nnet, point=True):
     img  = pad_image
     img_h, img_w, _ = img.shape
     pred = pred[pred[:, 0].astype(int) == 1]
-    overlay = np.zeros_like(img, np.uint8)
+    # overlay = np.zeros_like(img, np.uint8)
     overlay_rgb = img.copy()
-    GREEN = [0, 255, 0]
-    WHITE = [255, 255, 255]
+    point_xy = []
     for i, lane in enumerate(pred):
         lane = lane[1:]  # remove conf
         lower, upper = lane[0], lane[1]
@@ -102,20 +101,30 @@ def lane_detection(ori_image, mean, std, input_size, nnet, point=True):
         points[:, 0] = ((lane[0] / (ys - lane[1]) ** 2 + lane[2] / (ys - lane[1]) + lane[3] + lane[4] * ys -
                             lane[5]) * img_w).astype(int)
         points = points[(points[:, 0] > 0) & (points[:, 0] < img_w)]
-
+        point_xy.append(points)
         if point:
             for xxx, yyy in points:
-                cv2.circle(overlay, (xxx, yyy), 1, color=WHITE, thickness=4)
-                cv2.circle(overlay_rgb, (xxx, yyy), 1, color=GREEN, thickness=4)
+                # cv2.circle(overlay, (xxx, yyy), 1, color=WHITE, thickness=1)
+                cv2.circle(overlay_rgb, (xxx, yyy), 1, color=GREEN, thickness=1)
         else:
             for current_point, next_point in zip(points[:-1], points[1:]):
-                overlay = cv2.line(overlay, tuple(current_point), tuple(next_point), color=WHITE, thickness=4)
-                overlay_rgb = cv2.line(overlay_rgb, tuple(current_point), tuple(next_point), color=GREEN, thickness=4)
+                # overlay = cv2.line(overlay, tuple(current_point), tuple(next_point), color=WHITE, thickness=1)
+                overlay_rgb = cv2.line(overlay_rgb, tuple(current_point), tuple(next_point), color=GREEN, thickness=1)
 
-    return overlay, overlay_rgb
+    return overlay_rgb, point_xy
 
 def move_window(hwnd, x, y, n_width, n_height, b_repaint):
     win32gui.MoveWindow(hwnd, x - 7, y, n_width, n_height, b_repaint)
+
+def transform_point(pointxy, M):
+    result = []
+    for lanes in pointxy:
+        ps = lanes.shape[0]
+        pad = np.ones((ps,1), dtype=lanes.dtype)
+        pad = np.concatenate([lanes, pad], axis=1)
+        res = np.matmul(pad, M.T)
+        result.append(np.floor(res[:,:2]/res[:,-1][:, np.newaxis]).astype(np.int32))
+    return result
 
 if __name__ == "__main__":
     nnet, input_size, mean, std = get_lane_model()
@@ -129,7 +138,7 @@ if __name__ == "__main__":
     while(True):
         image =  np.array(ImageGrab.grab(bbox=bbox))
 
-        lane, mix = lane_detection(image, mean, std, input_size, nnet, True)
+        mix, points_xy = lane_detection(image, mean, std, input_size, nnet, True)
 
         # original pts
         pts_o = np.float32([[0, 200], [0, 600], [800, 600], [800, 200]]) # 这四个点为原始图片上数独的位置
@@ -138,17 +147,25 @@ if __name__ == "__main__":
         # get transform matrix
         M = cv2.getPerspectiveTransform(pts_o, pts_d)
         # apply transformation
-        process = cv2.warpPerspective(lane, M, (800, 400))
-        process = process[:,200:-200,:]
-        process = cv2.resize(process, (90, 90))
+        black_bg = np.zeros((400, 800, 3), dtype=np.float32)
+        # black_bg = cv2.warpPerspective(black_bg, M, (800, 400))
+        # print(black_bg.shape)
+        new_points = transform_point(points_xy, M)
+        for lanes in new_points:
+            for xxx,yyy in lanes:
+                if xxx<0 or yyy<0:
+                    continue
+                cv2.circle(black_bg, (xxx, yyy), 1, color=WHITE, thickness=3) 
+        cv2.imshow('proce', black_bg)
+        black_bg = black_bg[:,200:-200,:]
+        black_bg = cv2.resize(black_bg, (90, 90))
         # mix = cv2.cvtColor(mix, cv2.COLOR_BGR2BGRA)
         # process = cv2.cvtColor(process, cv2.COLOR_BGR2BGRA)
         w = 0.6
-        mix[-130:-40,-130:-40,:] = w*mix[-130:-40,-130:-40,:] + (1-w)*process
+        mix[-130:-40,-130:-40,:] = w*mix[-130:-40,-130:-40,:] + (1-w)*black_bg
         # mix[40:130,-130:-40,:] = np.clip(mix[40:130,-130:-40,:] + process, None, 255)
 
-        cv2.imshow('proce', process)
-        cv2.imshow('win', lane)
+        # cv2.imshow('win', lane)
         cv2.imshow('ori', mix)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
